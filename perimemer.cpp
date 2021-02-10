bdf65e55672adcf8a9d191254ae5d569ecb52183
Saraa7893 <78836178+Saraa7893@users.noreply.github.com>
2021-02-10 15:23:10 -0600

    Add files via upload

---
+++ perimemer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d, p;
+    a=14;
+    b=19;
+    c=12;
+    d=20;
+    p=a+b+c+d;
+    cout<<"A trapazoid has sides that are 14cm, 19cm, 12cm and 20cm, what's the perimeter?"<<endl;
+    cout<<"p="<<p;
+	
+  
+  
+    return 0;
