9d8abaf3a8aa57f6938802f2748c1c166c3c90ea
Saraa7893 <78836178+Saraa7893@users.noreply.github.com>
2021-02-16 11:44:42 -0600

    Add files via upload

---
+++ rectangle perimeter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x, y;
+	cout<<"Enter lenght = ";
+	cin>>x;
+	cout<<"Enter breadth = ";
+	cin>>y;
+	cout<<"the perimeter of the rectangle is = "<<(x+y)*2;
+	return 0;
+}  
