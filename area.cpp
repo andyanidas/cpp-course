803de5c089e6cb6b60c28392bf955f34615e4db4
Saraa7893 <78836178+Saraa7893@users.noreply.github.com>
2021-02-16 11:47:06 -0600

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x, y;
+	cout<<"Enter lenght = ";
+	cin>>x;
+	cout<<"Enter breadth = ";
+	cin>>y;
+	cout<<"The area of the rectangle is = "<<x*y;
+	return 0;
