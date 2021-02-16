72c67b2bff6103127a74e6aa2adffe1999c171f7
Saraa7893 <78836178+Saraa7893@users.noreply.github.com>
2021-02-16 11:40:28 -0600

    Add files via upload

---
+++ divitioin.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x, y;
+	cout<<"Enter the first number = ";
+	cin>>x;
+	cout<<"Enter the second number = ";
+	cin>>y;
+	cout<<"two numbers divided = "<<x/y;
+	return 0;
+} 
