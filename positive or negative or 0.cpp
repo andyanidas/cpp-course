113547c35de1af4d1abca1b305ca0fe63640111e
Saraa7893 <78836178+Saraa7893@users.noreply.github.com>
2021-02-17 19:12:31 -0600

    Add files via upload

---
+++ positive or negative or 0.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x;
+    cout<<"Enter any number: ";
+	cin>>x;
+	
+	if(x==0){
+	    cout<<"The number you entered is zero";
+	    if(x>0){
+	        cout<<"The number you entered is possitive";
+	   }else{
+	       cout<<"The number you entered is negative";
+	   }}
+
+
+
+	return 0;
+}
